2d98c9c327955ca6dab4adb16dd895edad356919
shubham423 <jethurishubham@gmail.com>
2022-01-02 11:03:19 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.0"
+                id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
