51b28f39182a92fd1b349782c1048ad4f4462159
shubham423 <jethurishubham@gmail.com>
2022-01-03 22:52:39 +0530

    status pages

--- src/main/kotlin/com/example/plugins/StatusPages.kt
+++ src/main/kotlin/com/example/plugins/StatusPages.kt
@@ -1,4 +1,17 @@
 package com.example.plugins
 
-class StatusPages {
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
+
+fun Application.configureStatusPages() {
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) {
+            call.respond(
+                message = "Page not Found.",
+                status = HttpStatusCode.NotFound
+            )
+        }
+    }
 }
