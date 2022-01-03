af9ef7bc7eca42f5056d8b0885bcfe4e70a9753f
shubham423 <jethurishubham@gmail.com>
2022-01-03 21:59:21 +0530

    data for endpoint

--- src/main/kotlin/com/example/routes/AllHeroes.kt
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -6,15 +6,22 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.koin.ktor.ext.inject
 
 fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by inject()
+
     get("/boruto/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
-
+            println("NEW PAGE: $page")
             require(page in 1..5)
 
-            call.respond(page)
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
         } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(success = false, message = "Only Numbers Allowed."),
