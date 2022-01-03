1963fa0aedbdb94e34f7dbff0305cbd6fc9c2e20
shubham423 <jethurishubham@gmail.com>
2022-01-03 21:19:39 +0530

    first end point

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,7 @@
 package com.example.plugins
 
+import com.example.routes.getAllHeroes
+import com.example.routes.root
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
@@ -9,8 +11,7 @@ import io.ktor.request.*
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-                call.respondText("Hello World!")
-            }
+        root()
+        getAllHeroes()
     }
 }
---
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -0,0 +1,30 @@
+package com.example.routes
+
+import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Route.getAllHeroes() {
+    get("/boruto/heroes") {
+        try {
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+
+            require(page in 1..5)
+
+            call.respond(page)
+        } catch (e: NumberFormatException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Only Numbers Allowed."),
+                status = HttpStatusCode.BadRequest
+            )
+        } catch (e: IllegalArgumentException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Heroes not Found."),
+                status = HttpStatusCode.NotFound
+            )
+        }
+    }
+}
